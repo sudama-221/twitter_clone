484bc2f083ba4ab18d4cb18acc50b66b73fbfb09
sudama-221 <hotaru4.ty@gmail.com>
2022-09-14 18:39:14 +0900

    なんそれ

---
+++ lib/widget/rounded_btn.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:twitter_clone2/util/color.dart';
+
+class RoundedBtn extends StatelessWidget {
+  final String btnText;
+  final Function onBtnPressed;
+
+  const RoundedBtn(
+      {super.key, required this.btnText, required this.onBtnPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: mainColor,
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          minimumSize: const Size(300, 50),
+        ),
+        onPressed: () {
+          // 画面遷移
+          onBtnPressed();
+        },
+        child: Text(
+          btnText,
+          style: const TextStyle(color: Colors.white, fontSize: 15),
+        ),
+      ),
+    );
+  }
+}
